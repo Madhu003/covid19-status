116eee7a0d49d14c550e6495e9f990f1da118110
Madhu003 <madhusudanarya003@gmail.com>
2020-05-15 23:25:52 +0530

    Updates

---
+++ precache-manifest.bd98c91fd7f3c22ff2a94597852b5f48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed442e35af5ae2d420151b79e65e509c",
+    "url": "/covid19-status/index.html"
+  },
+  {
+    "revision": "102066646e5bbf21a243",
+    "url": "/covid19-status/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6a18f946ae9f4a615b97",
+    "url": "/covid19-status/static/css/main.5db43e63.chunk.css"
+  },
+  {
+    "revision": "102066646e5bbf21a243",
+    "url": "/covid19-status/static/js/2.77b1d1f9.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/covid19-status/static/js/2.77b1d1f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a18f946ae9f4a615b97",
+    "url": "/covid19-status/static/js/main.93148dfe.chunk.js"
+  },
+  {
+    "revision": "ffd9fa470d07ab0498d3",
+    "url": "/covid19-status/static/js/runtime-main.b465361f.js"
+  }
+]);
