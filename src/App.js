258ceda2a2b05489faf9027dd49595ee493cafb2
Madhu Sudan Soni <madhusudanarya003@gmail.com>
2020-05-17 15:27:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,32 +2,26 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Home } from './Home';
 import { India } from './India';
 import { createBrowserHistory } from 'history';
 
 function App() {
   return (
-    <div className='App'>
-      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-        <a className='navbar-brand' href='#'>
-          COVID19 Status
-        </a>
-        <Nav activeKey='/active'>
-          <Nav.Item>
-            <Nav.Link href='/'>Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href='/india'>India</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </nav>
-      <Router history={createBrowserHistory()}>
-        <Route path='/' exact component={Home}></Route>
-        <Route path='/india' exact component={India}></Route>
-      </Router>
-    </div>
+    <Router history={createBrowserHistory()}>
+      <div className='App'>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+          <a className='navbar-brand' href='#'>
+            COVID19 Status
+          </a>
+          <Link to='/'>Home</Link>
+          <Link to='/india'>India</Link>
+        </nav>
+      </div>
+      <Route path='/' exact component={Home}></Route>
+      <Route path='/india' exact component={India}></Route>
+    </Router>
   );
 }
 
