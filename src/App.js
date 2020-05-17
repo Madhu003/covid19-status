ed3f2cd15c9bf822762238d7908f395c6b6473a2
Madhu Sudan Soni <madhusudanarya003@gmail.com>
2020-05-17 14:55:01 +0530

    wegrt
    rgh

--- src/App.js
+++ src/App.js
@@ -2,36 +2,38 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 import { Home } from './Home';
 import { India } from './India';
 import { createBrowserHistory } from 'history';
 
 function App() {
-    return (
-        <div className="App">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">COVID19 Status</a>
-                <Nav activeKey="/active">
-                    <Nav.Item>
-                        <Nav.Link href="/home">Home</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/india">India</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </nav>
-            <Router  history={createBrowserHistory()}>
-//                 <Switch>
-//                     <Route exact path="/" render={() => (
-//                         <Redirect to="home" />
-//                     )} />
-                    <Route path="/home" exact component={Home}></Route>
-                    <Route path="/india" exact component={India}></Route>
-//                 </Switch>
-            </Router>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+        <a class='navbar-brand' href='#'>
+          COVID19 Status
+        </a>
+        <Nav activeKey='/active'>
+          <Nav.Item>
+            <Nav.Link href='/home'>Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href='/india'>India</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </nav>
+      <Router history={createBrowserHistory()}>
+        <Route path='/home' exact component={Home}></Route>
+        <Route path='/india' exact component={India}></Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
